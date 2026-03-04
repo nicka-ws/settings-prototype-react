c963d95dc5e68d08158972159bbb56b7be475808
nicka-ws <142948578+nicka-ws@users.noreply.github.com>
2026-03-04 14:55:19 -0800

    vercel toolbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { mountVercelToolbar } from '@vercel/toolbar/vite'
 import './index.css'
 import App from './App.jsx'
 
@@ -8,3 +9,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+mountVercelToolbar()
